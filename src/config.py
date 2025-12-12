deab5d3ea5846137acfca17d2a08426288c8cf69
davi01mello <davismello.2006@gmail.com>
2025-12-12 16:14:03 -0300

    feat: Balanceamento de itens, reducao do Turbo e mecanica Anti-Camping, desneho de mapa

--- src/config.py
+++ src/config.py
@@ -7,21 +7,19 @@ SCREEN_HEIGHT = 600
 TITLE = "CIn Road: Rumo ao Diploma"
 
 # Configurações da Grade (Grid)
-# O tamanho do "pulo". 40x40 ou 50x50 costuma ser bom.
 BLOCK_SIZE = 50 
 GRID_WIDTH = SCREEN_WIDTH // BLOCK_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // BLOCK_SIZE
 
-# Cores (R, G, B)
+# Cores
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-GREEN = (34, 139, 34) # Cor da grama (provisório)
-BLUE = (0, 0, 255)    # Cor do Player (provisório)
+GREEN = (34, 139, 34)
+BLUE = (0, 0, 255)
 
 # FPS
 FPS = 60
-# Adicione no final do arquivo src/config.py
 
-# Meta: Quantos "passos" de grid até o CIn?
-# 50 passos * 50 pixels = mapa de 2500 pixels de altura
-GOAL_DISTANCE = 50
+# --- MUDANÇA AQUI ---
+# Meta aumentada para 100 passos (Jornada Longa)
+GOAL_DISTANCE = 100
