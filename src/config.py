f9a43eca9b70b4afce93021b4e0a981b5a869c9a
vnsfc <vnsfc@cin.ufpe.br>
2025-12-19 11:40:07 -0300

    config.py

--- src/config.py
+++ src/config.py
@@ -1,30 +1,22 @@
-# src/config.py
 import pygame
 
-# Configurações da Tela
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 TITLE = "CIn Road: Rumo ao Diploma"
 
-# Configurações da Grade (Grid)
 BLOCK_SIZE = 50 
 GRID_WIDTH = SCREEN_WIDTH // BLOCK_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // BLOCK_SIZE
 
-# Cores
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREEN = (34, 139, 34)
 BLUE = (0, 0, 255)
 
-# Sombra dos textos
 SHADOWCOLOR = (0, 0, 0)
 OFFSET = 4
 OFFSET_SMALL = 2
 
-# FPS
 FPS = 60
 
-# --- MUDANÇA AQUI ---
-# Meta aumentada para 100 passos (Jornada Longa)
 GOAL_DISTANCE = 100
